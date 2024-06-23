cfcaccc0348429c0a5e6670d28cf9212db6c6822
TejasvOnly <tejasvonly@gmail.com>
2024-06-22 19:49:07 -0700

    remove unused theme

--- config/ui/colorscheme.nix
+++ config/ui/colorscheme.nix
@@ -1,22 +1,4 @@
-{pkgs, ...}: let
-  github-theme = pkgs.vimUtils.buildVimPlugin {
-    name = "github-nvim-theme";
-    src = pkgs.fetchFromGitHub {
-      owner = "projekt0n";
-      repo = "github-nvim-theme";
-      rev = "66dcec1bd3d12f8b0bf3c885069888551a650049";
-      hash = "sha256-d23Jo2BI99HsNZnYuiCYy7q1iZ3PsG3KxG9OBp0E1aY=";
-    };
-  };
-in {
-  extraPlugins = [
-    github-theme
-    {
-      plugin = pkgs.vimPlugins.toggleterm-nvim;
-      config = "lua require'toggleterm'.setup()";
-    }
-  ];
-  #colorscheme = "github_dark";
+{
   colorschemes.nightfox = {
     enable = true;
     flavor = "nordfox";
